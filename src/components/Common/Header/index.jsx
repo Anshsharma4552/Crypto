0fb536ac70ce9588ce9ed43b13d4bd9537af26c5
Ansh Sharma <anshsharma2463@gmail.com>
2025-05-29 13:33:43 +0530

    Button

--- src/components/Common/Header/index.jsx
+++ src/components/Common/Header/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "./style.css" 
 import TemporaryDrawer from './drawer';
 import AnchorTemporaryDrawer from './drawer';
+import Button from '../Button';
 function Header() {
   return (
     <div className='navbar'>
@@ -10,7 +11,9 @@ function Header() {
         <a href='/'><p className='link'>Home</p></a>
         <a href='/'><p className='link'>Compare</p></a>
         <a href='/'><p className='link'>Watchlist</p></a>
-        <a href='/'><p className='link'>Dashboard</p></a>
+        <a href='#'>
+          <Button text={"Dashboard"} onClick={()=>console.log("Btn Clicked")}/>
+        </a>
       </div>
       <div className='mobile-drawer'>
         <AnchorTemporaryDrawer/>
