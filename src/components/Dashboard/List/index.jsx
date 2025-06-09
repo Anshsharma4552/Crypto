11c70221e515d31e7a9377873ff26c32b62493fd
Ansh Sharma <anshsharma2463@gmail.com>
2025-06-09 21:55:01 +0530

    changes

--- src/components/Dashboard/List/index.jsx
+++ src/components/Dashboard/List/index.jsx
@@ -2,18 +2,24 @@ import React from 'react'
 import "./style.css"
 import TrendingUpIcon from '@mui/icons-material/TrendingUp';
 import TrendingDownIcon from '@mui/icons-material/TrendingDown';
+import { Tooltip } from '@mui/material';
 function List({coin}) {
   return (
     <tr className='list-row'>
+        <Tooltip title="Coin Logo">
          <td className='td-image'>
             <img src={coin.image} className='coin-logo'/>
             </td>
+        </Tooltip>
+        <Tooltip title="Coin Info">
             <td>
             <div className='name-col'>
                 <p className='coin-symbol'>{coin.symbol}</p>
                 <p className='coin-name'>{coin.name}</p>
             </div>
         </td>
+        </Tooltip>
+        <Tooltip title="Price Change IN 24Hrs">
         {coin.price_change_percentage_24h > 0 ?(
         <td className='chip-flex'>
                 <div className='price-chip'>{coin.price_change_percentage_24h.toFixed(2)}%</div>
@@ -28,17 +34,24 @@ function List({coin}) {
                 </div>
             </td>
         )}
+        </Tooltip>
+        <Tooltip title="Current Price">
         <td>
-            <h3 className='coin-price' style={{color:coin.price_change_percentage_24h < 0 ? "var(--red)":"var(--green)"  }}>${coin.current_price.toLocaleString()}
+            <h3 className='coin-price td-center-align' style={{color:coin.price_change_percentage_24h < 0 ? "var(--red)":"var(--green)"  }}>${coin.current_price.toLocaleString()}
             </h3>
         </td>
+        </Tooltip>
+        <Tooltip title="Total Volume">
         <td>
-            <p className='total_volume'>{coin.total_volume.toLocaleString()}
+            <p className='total_volume td-right-align'>{coin.total_volume.toLocaleString()}
             </p>
         </td>
+        </Tooltip>
+        <Tooltip title="Market Cap">
         <td>
-            <p className='total_volume'>${coin.market_cap.toLocaleString()}</p>
+            <p className='total_volume td-right-align'>${coin.market_cap.toLocaleString()}</p>
         </td>
+        </Tooltip>
     </tr>
   )
 }
