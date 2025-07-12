37d69f8a00b525a92c369eec065b6ba0122ec711
Ansh Sharma <anshsharma2463@gmail.com>
2025-07-12 10:28:11 +0530

    ..

--- src/components/Common/Header/index.jsx
+++ src/components/Common/Header/index.jsx
@@ -11,7 +11,6 @@ function Header() {
       <div className='links'>
         <Link to='/'><p className='link'>Home</p></Link>
         <Link to='/compare'><p className='link'>Compare</p></Link>
-        <Link to='/watchlist'><p className='link'>Watchlist</p></Link>
         <Link to='/dashboard'>
           <Button text={"Dashboard"} onClick={()=>console.log("Btn Clicked")}/>
         </Link>
